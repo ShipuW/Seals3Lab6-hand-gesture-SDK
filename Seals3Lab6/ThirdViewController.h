3c5dbf449a500b4c80ab92e858ea0ab4c0b25ceb
sili.zsl <sili.zsl@alibaba-inc.com>
2015-08-05 17:24:31 +0800

    Init.

---
+++ Seals3Lab6/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  Seals3Lab6
+//
+//  Created by Veight Zhou on 8/5/15.
+//  Copyright (c) 2015 Veight Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
