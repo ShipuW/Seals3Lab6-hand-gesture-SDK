74acf5366e14e4435f31a78998238ea5accd2fc8
frank.wy <frank.wy@alibaba-inc.com>
2015-08-10 14:52:31 +0800

    工程文件

---
+++ Seals3Lab6/TBIndexPathCellModel.m
@@ -0,0 +1,13 @@
+//
+//  TBIndexPathCellModel.m
+//  Seals3Lab6
+//
+//  Created by Frank on 15/8/10.
+//  Copyright (c) 2015年 Veight Zhou. All rights reserved.
+//
+
+#import "TBIndexPathCellModel.h"
+
+@implementation TBIndexPathCellModel
+
+@end
