c774e41a2bac93bdd1de49088dd2d94749131f8d
决仕 <shipu.wsp@alibaba-inc.com>
2015-08-12 15:22:26 +0800

    add multi-pinch

--- Seals3Lab6/UICustomPinchGestureRecognizer.h
+++ Seals3Lab6/UICustomPinchGestureRecognizer.h
@@ -9,6 +9,15 @@
 
 #import <UIKit/UIKit.h>
 #import "TBGesture.h"
+
+@protocol TBCustomPinchGestureRecognizerDelegate <NSObject>
+
+
+- (void)gestureRecognizer:(UICustomPinchGestureRecognizer *)customPinchGestureRecognizer gestureType:(TBGestureType)type recognized:(BOOL)succeed;
+- (void)gestureRecognizer:(UICustomPinchGestureRecognizer *)customPinchGestureRecognizer gestureType:(TBGestureType)type gestureId:(int)gestureId recognized:(BOOL)succeed;
+
+@end
+
 @interface UICustomPinchGestureRecognizer:UIPinchGestureRecognizer
 {
     @package
@@ -24,6 +33,8 @@
 - (instancetype)initWithTarget:(id)target action:(SEL)action type:(TBGestureType)type;
 - (void)pinchAction:(UILongPressGestureRecognizer *)sender;
 
+
+@property (nonatomic) id<TBCustomPinchGestureRecognizerDelegate> recognizePinchDelegate;
 
 
 @end
