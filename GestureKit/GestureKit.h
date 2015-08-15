23e0846c1d4ba9ff31750a5bfca5b5634ab0cbb4
sili.zsl <sili.zsl@alibaba-inc.com>
2015-08-15 11:42:23 +0800

    添加 Framework

---
+++ GestureKit/GestureKit.h
@@ -0,0 +1,19 @@
+//
+//  GestureKit.h
+//  GestureKit
+//
+//  Created by Veight Zhou on 8/14/15.
+//  Copyright (c) 2015 Veight Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GestureKit.
+FOUNDATION_EXPORT double GestureKitVersionNumber;
+
+//! Project version string for GestureKit.
+FOUNDATION_EXPORT const unsigned char GestureKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GestureKit/PublicHeader.h>
+
+
